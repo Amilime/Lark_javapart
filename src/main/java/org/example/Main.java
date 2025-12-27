5afb2c9861a00ed1369b7c04bb7f8dcb373d6ca0
Amilime <1303348976@qq.com>
2025-12-27 22:33:26 +0800

    初步实现协同系统

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// 这是三合一，
+// 这是三合一
 // 说明是主配置类，
 // 检查pom.xml是否有数据库连接和tomcat，
 // 最后自动扫描当前包和子包的文件
